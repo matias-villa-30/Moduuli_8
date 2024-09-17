b0c83e72006cc2e634e63706dc796d47a8203e29
villa <matias.villalemes@metropolia.fi>
2024-09-17 19:27:45 +0300

    harj 1 korjattu

--- 1.py
+++ 1.py
@@ -3,7 +3,7 @@ import mysql.connector
 connection = mysql.connector.connect(
          host='localhost',
          port= 3306,
-         database='flight_game',
+         database='lp',
          user='root',
          password='',
          autocommit=True
@@ -11,15 +11,14 @@ connection = mysql.connector.connect(
 
 icao_koodi = input("Anna ICAO koodi: ")
 
-koodi_tietokannassa = "SELECT name FROM airport WHERE ident=%s"
+koodi_tietokannassa = "SELECT name, municipality FROM airport WHERE ident=%s"
 cursor = connection.cursor()
 cursor.execute(koodi_tietokannassa, (icao_koodi,))
 
 results = cursor.fetchone()
 
 if results:
-    for item in results:
-        print(f"Lentoasema on: {item}")
+    print(f"Lentoasema on {results[0]}, paikka on {results[1]}")
 
 
 else:
